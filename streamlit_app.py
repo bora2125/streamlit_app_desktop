7fe8038b32a3e33a0512d619b4d95ee24526e8b7
bora2125 <86435918+bora2125@users.noreply.github.com>
2024-03-01 15:12:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,12 @@
-# streamlit run "C:\Users\cveas\OneDrive - ACEROS AZA S.A\OneDrive_v1\Escritorio\GITUPLOADS\streamlit_mysql_connection\streamlit_app.py"
-
-
 import streamlit as st
 import pyodbc
-
-# ServerName = st.secrets["server"]
-# MSQLDatabase = st.secrets["database"]
-# username = st.secrets["username"]
-# password = st.secrets["password"]
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
     return pyodbc.connect(
-        "DRIVER={ODBC Driver 10 for SQL Server};SERVER="
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + st.secrets["server"]
         + ";DATABASE="
         + st.secrets["database"]
